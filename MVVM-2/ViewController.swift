4512b2b288d43ffc309086d0dab68476ab53cd58
emmaderbe <emma-derbe@mail.ru>
2022-06-27 14:27:24 +0300

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Дербе Эмма on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
